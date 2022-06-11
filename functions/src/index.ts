8be60889dd1f7a5bcb8d2f4737fdd92a2e64ee6d
Andrew Dimmer <contactandrewd@gmail.com>
2022-06-11 03:21:49 -0400

    chore(functions): Configure the Basic Express App
    Completes #4.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,23 @@
+import express = require("express");
+import type { NextFunction, Request, Response } from "express";
 import * as functions from "firebase-functions";
+import cors = require("cors");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Initialize the Express App for the back end
+export const media_metadata_manager = (() => {
+  const app = express();
+
+  // Automatically allow cross-origin requests
+  // Required to connect the localhost site to the emulator
+  // TODO: Fix this for production in the future
+  app.use(cors({ origin: true }));
+
+  const helloWorld = (req: Request, res: Response, next: NextFunction) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    res.send({ data: "Hello from Firebase!" });
+  };
+
+  app.all("/", helloWorld);
+
+  return functions.https.onRequest(app);
+})();
