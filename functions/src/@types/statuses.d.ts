7df583965fb660c38a4208b858cec2a4ca2e84d0
Andrew Dimmer <contactandrewd@gmail.com>
2022-06-12 04:51:35 -0400

    feat(functions): Define Typescript Status and DateTime Datatypes
    Working on #15 and #16.

---
+++ functions/src/@types/statuses.d.ts
@@ -0,0 +1,37 @@
+declare interface StatusData {
+  id: string;
+  createTimestamp: number;
+  confirmTimestamp?: number;
+  countryId: string;
+  postalCodeId: string;
+  beachId: string;
+  status: StatusOptions;
+  notes?: string;
+}
+
+declare interface Status extends StatusData {
+  createDateTime: DateTime;
+  confirmDateTime?: DateTime;
+}
+
+declare interface CreateStatusInput {
+  countryId: string;
+  postalCodeId: string;
+  beachId: string;
+  status: StatusOptions;
+  notes?: string;
+}
+
+declare interface ConfirmStatusInput {
+  countryId: string;
+  postalCodeId: string;
+  beachId: string;
+  statusId: string;
+}
+
+declare enum StatusOptions {
+  CLOSED,
+  ADVISORY,
+  NOTICE,
+  OPEN,
+}
